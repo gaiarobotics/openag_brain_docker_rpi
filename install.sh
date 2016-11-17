1620bdf04228f3109fce32d752688da78d5e452d
Andres Gomez Urquiza <andres@fractal.ai>
2016-11-16 18:28:08 -0600

    point to in house repo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone http://github.com/OpenAgInitiative/openag_brain_docker_rpi
+git clone http://github.com/gaiarobotics/openag_brain_docker_rpi
 cd openag_brain_docker_rpi
 sh install_docker.sh
 docker-compose up -d
